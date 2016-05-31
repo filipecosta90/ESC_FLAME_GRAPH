a4a2c9a9ddc5c6d3f66158dbc420964aad1214ed
filipecosta90 <filipecosta.90@gmail.com>
2016-05-31 03:09:43 +0100

    added commands to separated files

--- get_times_inter.sh
+++ get_times_inter.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
 sample_size=50
 
 for (( seq_num=1; seq_num <= $sample_size; ++seq_num ))
 do
-  time ./large_interchange > "time_interchange.csv"
+  time -p  ./large_interchange 
 done
---
+++ get_times_naive.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sample_size=50
+
+for (( seq_num=1; seq_num <= $sample_size; ++seq_num ))
+do
+  time -p  ./large_naive 
+done
